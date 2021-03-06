109134b5a963796b04fee3a98ea3e751f9b57878
thaiminhpv <thaiminhpv@gmail.com>
2021-03-06 15:13:25 +0700

    Update template.py to use input( ) as input

--- Template/Template.py
+++ Template/Template.py
@@ -1,32 +1,38 @@
 import sys
+import os
 import math
 
-# Config 
+# Config
 TEST_MODE = True
-NUM_OF_TEST = 1
+NUM_OF_TEST = 3
+
 
 # Solver
-def solver(content):
-    pass
+def solver(get_line):
+    input = get_line  # <-- change TEST_MODE to TRUE and comment out this line
+
+    ...
+    print(input())
+    ...
 
 
 def solveByPath(path):
-    with open(path) as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
-    solver(content)
+    with open(path) as f: content = f.readlines()
+    holder = (x.strip() for x in content)
+    solver(lambda: next(holder))
+
 
 if __name__ == '__main__':
     if TEST_MODE:
+        IS_LINUX = True
         for i in range(1, NUM_OF_TEST + 1):
-            problem = sys.argv[0].replace('.py', '')
+            if IS_LINUX:
+                problem = sys.argv[0].replace('.py', '')
+            else:
+                problem = os.path.basename(__file__).replace('.py', '')
             path = f'testcase/samples-{problem}/{i}.in'
             print('----------')
             print('TestCase ' + str(i))
             solveByPath(path)
-
     else:
         solver(None)
-
-
-
