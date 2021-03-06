56e26c7d24d0ac7692fd64ee4c75243dab08c675
dan3002 <anhndfx00424@funix.edu.vn>
2021-03-06 15:35:16 +0700

    [CHANGE] Update Python Template

--- Template/Template.py
+++ Template/Template.py
@@ -1,19 +1,14 @@
-import sys
 import os
 import math
 
 # Config
 TEST_MODE = True
+# TEST_MODE = False
 NUM_OF_TEST = 3
 
-
 # Solver
-def solver(get_line):
-    input = get_line  # <-- change TEST_MODE to TRUE and comment out this line
-
-    ...
-    print(input())
-    ...
+def solver(input):
+    pass
 
 
 def solveByPath(path):
@@ -24,15 +19,11 @@ def solveByPath(path):
 
 if __name__ == '__main__':
     if TEST_MODE:
-        IS_LINUX = True
         for i in range(1, NUM_OF_TEST + 1):
-            if IS_LINUX:
-                problem = sys.argv[0].replace('.py', '')
-            else:
-                problem = os.path.basename(__file__).replace('.py', '')
+            problem = os.path.basename(__file__).replace('.py', '')
             path = f'testcase/samples-{problem}/{i}.in'
             print('----------')
             print('TestCase ' + str(i))
             solveByPath(path)
     else:
-        solver(None)
+        solver(input)
