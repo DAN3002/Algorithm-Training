44dfd142504c157598ca784b5e151c3a9511d1de
Thaiminhpv PC <thaiminhpv@gmail.com>
2021-03-07 09:15:07 +0700

    minimize template

--- FPT University - Hackathon 2021 - Round 2/H.py
+++ FPT University - Hackathon 2021 - Round 2/H.py
@@ -1,30 +1,21 @@
 import os
 import math
 
-# Config
 TEST_MODE = True
 # TEST_MODE = False
 NUM_OF_TEST = 3
 
 
-# Solver
 def solver(input):
     ...
-
-
-def solveByPath(path):
-    with open(path) as f: content = f.readlines()
-    holder = (x.strip() for x in content)
-    solver(lambda: next(holder))
 
 
 if __name__ == '__main__':
     if TEST_MODE:
         for i in range(1, NUM_OF_TEST + 1):
-            problem = os.path.basename(__file__).replace('.py', '')
-            path = f'testcase/samples-{problem}/{i}.in'
-            print('----------')
-            print('TestCase ' + str(i))
-            solveByPath(path)
+            print(f'----------\nTestCase {i}')
+            with open('testcase/samples-{}/{}.in'.format(os.path.basename(__file__).replace('.py', ''), i)) as f: content = f.readlines()
+            holder = (_.strip() for _ in content)
+            solver(lambda: next(holder))
     else:
         solver(input)
