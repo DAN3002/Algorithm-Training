aa6db8ac09ab1821348708d2db422a16fda7275e
Thaiminhpv PC <thaiminhpv@gmail.com>
2021-03-08 23:31:09 +0700

    add vector projection (simple PCA)

--- Template/util/vector.py
+++ Template/util/vector.py
@@ -1,15 +1,6 @@
 import math
 
 
-def linspace(start, stop, n):
-    if n == 1:
-        yield stop
-        return
-    h = (stop - start) / (n - 1)
-    for i in range(n):
-        yield start + h * i
-
-
 def dot(vector1, vector2):
     return sum(map(math.prod, zip(vector1, vector2)))
 
@@ -18,3 +9,29 @@ def outer_product(vector1, vector2):
     vector1 = [[_] for _ in vector1]  # transpose
     vector2 = [vector2]
     return [[sum(m * n for m, n in zip(i, j)) for j in zip(*vector2)] for i in vector1]
+
+
+def flatten(vector):
+    # roll 2d to 1d
+    return [_ for __ in vector for _ in __]
+
+
+def vector_projection_to_vector(u, v):
+    # project u to v
+    _ = dot(u, v) / dot(v, v)
+    return [e * _ for e in v]
+
+
+def vector_projection_plane(u, n):
+    # project u to plane P has vector phap tuyen n
+    _ = dot(u, n) / dot(n, n)
+    return [u[i] - n[i] * _ for i in range(len(u))]
+
+
+def linspace(start, stop, n):
+    if n == 1:
+        yield stop
+        return
+    h = (stop - start) / (n - 1)
+    for i in range(n):
+        yield start + h * i
