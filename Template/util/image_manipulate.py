6eb46fae5848dbe1ce2639976cdec763f0780e2a
Thaiminhpv PC <thaiminhpv@gmail.com>
2021-03-10 18:13:57 +0700

    k-mean! self-implement

--- Template/util/image_manipulate.py
+++ Template/util/image_manipulate.py
@@ -1,3 +1,8 @@
+import random
+import math
+from statistics import mean
+
+
 def mean_threshold(image):
     """
     image is n x m x 1
@@ -10,8 +15,20 @@ def mean_threshold(image):
     return image
 
 
-def k_mean(k):
-    from random import randint
-
-    # TODO
-    ...
+def k_mean(data, k: int, iteration: int, cost_distance=lambda a, b: abs(a - b)):
+    # cost_distance=lambda a, b: math.dist(a, b)
+    centroids = random.sample(data, k)
+    for _ in range(iteration):
+        centroid_holder = [[] for __ in range(k)]
+        # cluster assignment
+        for point in data:
+            centroid_index, min_distance = min(
+                ((j, cost_distance(point, centroid)) for j, centroid in enumerate(centroids)), key=lambda _: _[1])
+            centroid_holder[centroid_index].append(point)
+        # move cluster to mean
+        for i, assigned_data in enumerate(centroid_holder):
+            if assigned_data:
+                centroids[i] = mean(assigned_data)
+            else:
+                centroids[i] = random.choice(data)
+    return centroids, centroid_holder
