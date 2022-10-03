9f02c5839588737c38052382e29f22514f46376e
D.A.N_3002 <dinhanh300229@gmail.com>
2022-10-03 11:10:20 +0700

    Init hackerank

---
+++ Hacker Rank/Tutorials/10 Days of Statistics/s10-weighted-mean.py
@@ -0,0 +1,28 @@
+# Day 0: Weighted Mean
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+	pass
+    # Write your code here
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
+
