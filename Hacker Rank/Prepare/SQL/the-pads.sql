afdb3c85b139002816872061e590f68a9e96ad20
DAN3002 <dinhanh300229@gmail.com>
2022-12-12 18:51:11 +0700

    Add SQL Hackerank

---
+++ Hacker Rank/Prepare/SQL/the-pads.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(Name, '(', LEFT(Occupation, 1), ')')
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(*), ' ', LOWER(Occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(*), Occupation;
